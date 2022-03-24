9875d55cf05a086486bd826430c5bd50b24eb9cd
Opaul Chanlongsawettakun <Opaul.chan@gmail.com>
2022-03-24 12:25:46 +0700

    "data"

---
+++ data.js
@@ -0,0 +1,9 @@
+const data = [
+  {
+    id: 1,
+    activityName: "running with dog",
+    activityType: "running",
+    activityDuration: "60 mins",
+    activityDescription: "happy and fun",
+  },
+];
