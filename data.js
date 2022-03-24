bcd7aad7708713cb7f7c5414465575c63ed193b6
Opaul Chanlongsawettakun <Opaul.chan@gmail.com>
2022-03-24 12:28:26 +0700

    "create data"

--- data.js
+++ data.js
@@ -6,4 +6,25 @@ const data = [
     activityDuration: "60 mins",
     activityDescription: "happy and fun",
   },
+  {
+    id: 2,
+    activityName: "running with dog",
+    activityType: "running",
+    activityDuration: "60 mins",
+    activityDescription: "happy and fun",
+  },
+  {
+    id: 3,
+    activityName: "running with dog",
+    activityType: "running",
+    activityDuration: "60 mins",
+    activityDescription: "happy and fun",
+  },
+  {
+    id: 4,
+    activityName: "running with dog",
+    activityType: "running",
+    activityDuration: "60 mins",
+    activityDescription: "happy and fun",
+  },
 ];
