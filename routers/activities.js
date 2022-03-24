0cc515be7de19b7191438f810a2ff9d67a50ace6
Opaul Chanlongsawettakun <Opaul.chan@gmail.com>
2022-03-24 15:15:54 +0700

    "set get method"

--- routers/activities.js
+++ routers/activities.js
@@ -3,25 +3,37 @@ const router = express.Router();
 
 const activities = require("../data");
 
-router.get("/apis/records", (req, res) => {
-  res.json(activityAll);
-  console.log(activityAll);
+router.get("/", (req, res) => {
+  res.json(activities);
 });
 
 //get record by id
-router.get("/apis/records/:id", (req, res) => {
-  res.send("GET ONE API");
+// router.get("/:id", (req, res) => {
+//   const activityId = Number.parseInt(req.params.id, 10);
+//   const activity = activities.find((activity = activity.id = activityId));
+
+//   res.json(activity);
+// });
+router.get("/:id", (req, res) => {
+  const activityId = Number.parseInt(req.params.id, 10);
+  const activity = activities.find(function (activity) {
+    if (activity.id === activityId) {
+      res.json(activities);
+    } else {
+      res.send("404 Not found");
+    }
+  });
 });
 
-router.post("/apis/records", (req, res) => {
+router.post("/", (req, res) => {
   res.send("POST API");
 });
 
-router.put("/apis/records/:id", (req, res) => {
+router.put("/:id", (req, res) => {
   res.send("PUT API");
 });
 
-router.delete("/apis/records/:id", (req, res) => {
+router.delete("/:id", (req, res) => {
   res.send("DELETE API");
 });
 
